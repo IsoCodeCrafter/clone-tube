6b5881a60ce63407fc701e867a24946abd36fdc2
ISMAIL DOGAN <134277808+IsoCodeCrafter@users.noreply.github.com>
2023-10-22 15:01:13 +0300

    Delete OneDrive/Desktop/NetflixClone directory

--- OneDrive/Desktop/NetflixClone/src/redux/actions/ActionsTypes.js
+++
@@ -1,5 +0,0 @@
-export const actionTypes = {
-    SET_MOVIES : "SET_MOVIE",
-    SET_LOADING : "SET_LOADING",
-    SET_CATEGORIES: "SET_CATEGORİES"
-}
--- OneDrive/Desktop/NetflixClone/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import { movieReducer } from "./reducers/movieReducer";
-import thunk from 'redux-thunk'
-
-const rootReducer = combineReducers({
-  movieReducer,
-});
-
-export default createStore(rootReducer,applyMiddleware(thunk));
